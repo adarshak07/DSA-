23eb3f63a13050b2b22aa555c2f3bed094918865
Adarsh Kumar <93122509+adarshak07@users.noreply.github.com>
2023-03-27 14:33:14 +0530

    Create experiment 2.2 python.py

---
+++ experiment 2.2 python.py
@@ -0,0 +1,19 @@
+l1 =[10,3,3,5,6]  #list is ordered and changeable
+print(l1) # duplicate value can take
+#list can take any type of value 
+#print(l1[3])
+print(l1[:4]) 
+print(l1[-3:]) #slicing also used in list like string 
+
+print(len(l1))  
+print(min(l1))
+print(max(l1))
+
+print(4 in l1)
+
+l2=[4,5,2,4]
+l3=l1+l2 #concate 2 list 
+print(l3)
+
+print(l2*3) 
+
