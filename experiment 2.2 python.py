0e1bf46a6bc4de4e9287f14e1bdf17aba910f5b9
Adarsh Kumar <93122509+adarshak07@users.noreply.github.com>
2023-04-03 14:36:20 +0530

    Update experiment 2.2 python.py

--- experiment 2.2 python.py
+++ experiment 2.2 python.py
@@ -1,3 +1,5 @@
+
+
 l1 =[10,3,3,5,6]  #list is ordered and changeable
 print(l1) # duplicate value can take
 #list can take any type of value 
@@ -16,4 +18,42 @@ l3=l1+l2 #concate 2 list
 print(l3)
 
 print(l2*3) 
+
+print(l1.count(3))
+
+#append all lements of l2 in l1 
+l1.extend(l2)
+print(l1)
+
+print(l1.index(4)) #gives index of fisrst occuranvce 
+
+l1.insert(2,1000) #inserting a elemny at a given index 
+print(l1)
+
+l1.remove(3) #removes frist occurnece of that element , 3 is the element value 
+print(l1)
+
+l1.reverse()
+print(l1)
+
+l1.sort()
+print(l1)
+
+l1.pop() #removes last element 
+print(l1)
+
+l1.pop(3) #removes third index value using pop 
+print(l1)
+
+#del l1 #whole list is deleted
+
+del l1[0]  #removes oth index element using del 
+print(l1)
+
+l1.clear()  #clear removes all elements of the list but the list is nt deleted 
+print(l1)
+
+
+
+
 
