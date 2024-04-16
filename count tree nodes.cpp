3a1ae861ec85186571b9bfb7c6793af7a8fd8bba
Adarsh Kumar <93122509+adarshak07@users.noreply.github.com>
2024-04-16 13:54:44 +0530

    Create count tree nodes.cpp

---
+++ count tree nodes.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int solve(TreeNode* root){
+        if(root==NULL) return 0;
+        return 1+solve(root->left)+solve(root->right);
+    }
+    int countNodes(TreeNode* root) {
+        return solve(root);
+    }
+};
